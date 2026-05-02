e236c7a49b9a640315bd38996ea889c920f6d0e3
imalaana8-sketch <imalaana8@gmail.com>
2026-05-03 00:07:22 +0800

    Update web_alor.py

--- web_alor.py
+++ web_alor.py
@@ -8,7 +8,7 @@ genai.configure(api_key=API_KEY)
 
 # Mencoba menggunakan model yang paling tersedia (Flash Terbaru)
 try:
-    model = genai.GenerativeModel('gemini-1.5-flash-latest')
+    model = genai.GenerativeModel('gemini-1.5-flash')
 except:
     model = genai.GenerativeModel('gemini-pro')
 
